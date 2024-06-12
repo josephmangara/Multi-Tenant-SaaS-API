e1cab107fa307bd9716d380bf2c45cda5a5af78c
josephmangara <josephmangara2@gmail.com>
2024-06-12 17:39:02 +0300

    updated tenants to landlords

---
+++ app/models.py
@@ -0,0 +1,30 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+from flask_bcrypt import Bcrypt 
+from sqlalchemy_serializer import SerializerMixin
+
+db = SQLAlchemy()
+bcrypt = Bcrypt() 
+
+
+# User and Tenant Models
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime(), server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(), onupdate=db.func.now())
+        
+    landlord_id = db.Column(db.Integer, db.ForeignKey('landlords.id'))
+
+class Landlord(db.Model):
+    __tablename__ = "landlords"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime(), server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(), onupdate=db.func.now())
+
+    users = db.relationship('User', backref="landlords")
