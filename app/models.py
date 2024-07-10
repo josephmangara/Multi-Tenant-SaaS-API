fcfca9a714601dd75fbb5439038fa1934d4768d0
josephmangara <josephmangara2@gmail.com>
2024-07-10 23:54:57 +0300

    properties class

--- app/models.py
+++ app/models.py
@@ -29,10 +29,33 @@ class Landlord(db.Model):
 
     users = db.relationship('User', backref="landlords")
 
+# class Location(db.Model):
+#     __tablename__ = "locations"
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     address = db.Column(db.String)
+
+#  consider changing location to properties 
+class Property(db.Model):
+    __tablename__ = "properties"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float, nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.id'))
+    landlord_id = db.Column(db.Integer, db.ForeignKey('landlords.id'))
+
+    location = db.relationship('Location', backref='properties')
+    landlord = db.relationship('Landlord', backref='properties')
+
 class Location(db.Model):
     __tablename__ = "locations"
 
     id = db.Column(db.Integer, primary_key=True)
-    address = db.Column(db.String)
+    address = db.Column(db.String(200), nullable=False)
+    city = db.Column(db.String(80), nullable=False)
+    state = db.Column(db.String(80), nullable=False)
+    zip_code = db.Column(db.String(20), nullable=False)
 
-#  consider changing location to properties 
+    properties = db.relationship('Property', backref='locations')
